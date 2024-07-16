a4eee50ca0bbe11ebb3c208d7b8eaa6669ad5549
Lbarragan <luismab95@gmail.com>
2024-07-16 16:39:22 -0500

    avance  maquetado  drawer

--- src/app/modules/admin/pages/detail/drawer/drawer.component.ts
+++ src/app/modules/admin/pages/detail/drawer/drawer.component.ts
@@ -1,10 +1,17 @@
+import { NgClass } from '@angular/common';
 import {
     ChangeDetectionStrategy,
     Component,
+    EventEmitter,
     Input,
     OnInit,
+    Output,
     ViewEncapsulation,
 } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router, RouterLink } from '@angular/router';
 import { Subject } from 'rxjs';
 
 @Component({
@@ -13,17 +20,27 @@ import { Subject } from 'rxjs';
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
-    imports: [],
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+        NgClass,
+        RouterLink,
+        MatTooltipModule,
+    ],
 })
 export class PagesDrawerComponent implements OnInit {
     @Input() page: any;
     @Input() micrositie: any;
+    @Input() template: any;
+    @Output() fullscreemToggle: EventEmitter<boolean> =
+        new EventEmitter<boolean>();
+    preview: string;
     private _unsubscribeAll: Subject<any> = new Subject<any>();
 
     /**
      * Constructor
      */
-    constructor() {}
+    constructor(private _router: Router) {}
 
     // -----------------------------------------------------------------------------------------------------
     // @ Lifecycle hooks
@@ -41,5 +58,27 @@ export class PagesDrawerComponent implements OnInit {
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next(null);
         this._unsubscribeAll.complete();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    toggleFullscreen() {
+        this.fullscreemToggle.emit(true);
+    }
+
+    /**
+     * Ir atras
+     *
+     */
+    goToBack() {
+        if (this.micrositie) {
+            this._router.navigateByUrl('/admin/modules/microsities/detail', {
+                state: { micrositie: this.micrositie },
+            });
+        } else {
+            this._router.navigateByUrl('/admin/modules/pages');
+        }
     }
 }
--- src/app/modules/admin/templates/detail/drawer/drawer.component.ts
+++ src/app/modules/admin/templates/detail/drawer/drawer.component.ts
@@ -1,10 +1,17 @@
+import { NgClass } from '@angular/common';
 import {
     ChangeDetectionStrategy,
     Component,
+    EventEmitter,
     Input,
     OnInit,
+    Output,
     ViewEncapsulation,
 } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
 import { Subject } from 'rxjs';
 
 @Component({
@@ -13,10 +20,18 @@ import { Subject } from 'rxjs';
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
-    imports: [],
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+        NgClass,
+        RouterLink,
+        MatTooltipModule,
+    ],
 })
 export class TemplatesDrawerComponent implements OnInit {
     @Input() template: any;
+    @Output() fullscreemToggle: EventEmitter<boolean> = new EventEmitter<boolean>();
+    preview: string;
     private _unsubscribeAll: Subject<any> = new Subject<any>();
 
     /**
@@ -40,5 +55,13 @@ export class TemplatesDrawerComponent implements OnInit {
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next(null);
         this._unsubscribeAll.complete();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    toggleFullscreen() {
+        this.fullscreemToggle.emit(true);
     }
 }
